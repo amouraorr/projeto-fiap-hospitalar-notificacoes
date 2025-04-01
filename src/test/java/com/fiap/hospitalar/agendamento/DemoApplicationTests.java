d37d41bfc794af2e4aa861787f24b1fd57dc24d7
Alexsandro <amouraorr@yahoo.com.br>
2025-04-01 11:13:58 -0300

    Implementação iniciais.

---
+++ src/test/java/com/fiap/hospitalar/agendamento/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.hospitalar.agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
